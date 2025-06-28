71850a2d6a96df7c40c60f0799c2a59fff41373f
AryanVarmora <aryanvarmora66@gmail.com>
2025-06-28 22:42:53 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aryan/', 
+  base: '/', // 👈 CRUCIAL for GitHub Pages!
 });
